defd8dc626d39f76fc8b8b851de37d8574a0f6cd
abubakar-dev-1 <mianabubaka007@gmail.com>
2024-08-26 13:12:11 +0500

    inti

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+      <div className='bg-black flex justify-center text-sm gap-3 p-1 '>
+        <p>Summe Sale For All Swim Suits And Free Express Delivery - OFF 50%!</p>
+        <button className='underline'>ShopNow</button>
+      </div>
+      <div></div>
+    </>
+  )
+}
